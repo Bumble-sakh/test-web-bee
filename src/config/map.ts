2499954a58b683bc2e88267695f28df9576ae768
Bumble <bumble.sakh@gmail.com>
2023-05-24 22:34:11 +1100

    feat: add map page

---
+++ src/config/map.ts
@@ -0,0 +1,7 @@
+export const LOCATION = { center: [46.63334, 142.779911], zoom: 16 };
+
+export const POINT = {
+  coordinates: [46.63334, 142.779911],
+  title: 'Тут я живу',
+  icon: 'islands#darkOrangeStretchyIcon',
+};
