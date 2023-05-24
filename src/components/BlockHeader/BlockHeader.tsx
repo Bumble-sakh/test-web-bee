349f11e8bf3e73ddb11c93b07ddb36983677ef49
Igor Asessorov <71071594+Bumble-sakh@users.noreply.github.com>
2023-05-24 18:39:05 +1100

    refactor: basic block (#9)

--- src/components/BlockHeader/BlockHeader.tsx
+++ src/components/BlockHeader/BlockHeader.tsx
@@ -20,40 +20,37 @@ const BlockHeader: React.FC<BlockProps> = ({ collapse = false, update = false, c
   const isControls = collapse || update || close;
 
   return (
-    <section className={styles.section}>
-      <div className={styles.header}>
-        {isTitle && <h3 className={styles.title}>{title}</h3>}
+    <div className={styles.header}>
+      {isTitle && <h3 className={styles.title}>{title}</h3>}
 
-        {isTime && (
-          <div>
-            <img src={timeIcon} alt="time" />
-            <span>{time} ago</span>
-          </div>
-        )}
+      {isTime && (
+        <div>
+          <img src={timeIcon} alt="time" />
+          <span>{time} ago</span>
+        </div>
+      )}
 
-        {isControls && (
-          <div className="controls">
-            {collapse && (
-              <button className={styles.collapse}>
-                <img src={collapseIcon} alt="collapse" />
-              </button>
-            )}
-            {update && (
-              <button className={styles.update}>
-                <img src={updateIcon} alt="update" />
-              </button>
-            )}
-            {close && (
-              <button className={styles.close}>
-                <img src={closeIcon} alt="close" />
-              </button>
-            )}
-          </div>
-        )}
-      </div>
-      <span className={styles.timer}></span>
-    </section>
+      {isControls && (
+        <div className={styles.controls}>
+          {collapse && (
+            <button className={styles.collapse}>
+              <img src={collapseIcon} alt="collapse" />
+            </button>
+          )}
+          {update && (
+            <button className={styles.update}>
+              <img src={updateIcon} alt="update" />
+            </button>
+          )}
+          {close && (
+            <button className={styles.close}>
+              <img src={closeIcon} alt="close" />
+            </button>
+          )}
+        </div>
+      )}
+    </div>
   );
 };
 
-export default BlockHeader;
+export default React.memo(BlockHeader);
