ab1e4feeea289beadc39fb0df51eac836ea29fcf
Igor Asessorov <71071594+Bumble-sakh@users.noreply.github.com>
2023-05-24 22:43:18 +1100

    Feat/map (#11)
    * feat: add loader
    
    * feat: add deps
    
    * feat: add map page

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import BasicBlock from '@components/BasicBlock';
+import Loader from './Loader';
+import { YMaps, Map as YMap, Placemark } from '@pbe/react-yandex-maps';
+import { LOCATION, POINT } from '@config/map';
+import styles from './Map.module.scss';
 
 const Map: React.FC = () => {
-  return <section>Map</section>;
+  const [isLoading, setIsLoading] = useState(true);
+
+  const onLoadHandler = (): void => {
+    setIsLoading(false);
+  };
+
+  return (
+    <BasicBlock title="Map">
+      <div className={styles.map}>
+        <YMaps>
+          {isLoading && <Loader />}
+          <YMap defaultState={LOCATION} width={'100%'} height={480} onLoad={onLoadHandler}>
+            <Placemark
+              geometry={POINT.coordinates}
+              properties={{ iconContent: POINT.title }}
+              options={{ preset: POINT.icon }}
+            />
+          </YMap>
+        </YMaps>
+      </div>
+    </BasicBlock>
+  );
 };
 
 export default Map;
