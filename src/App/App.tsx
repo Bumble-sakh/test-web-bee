c1472c9aa84062a160f390b1587ed01a2aae50c5
Bumble <bumble.sakh@gmail.com>
2023-05-24 18:09:58 +1100

    feat: add timer

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,19 +6,23 @@ import Timer from '@pages/Timer';
 import ROUTES from '@config/routes';
 import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
+export const TimerContext = React.createContext(0);
+
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path={ROUTES.index} element={<Layout />}>
-          <Route path={ROUTES.resume} element={<Resume />} />
-          <Route path={ROUTES.map} element={<Map />} />
-          <Route path={ROUTES.timer} element={<Timer />} />
-          <Route path={ROUTES.all} element={<Navigate to={ROUTES.index} replace />} />
-          <Route index element={<Resume />} />
-        </Route>
-      </Routes>
-    </Router>
+    <TimerContext.Provider value={Date.now()}>
+      <Router>
+        <Routes>
+          <Route path={ROUTES.index} element={<Layout />}>
+            <Route path={ROUTES.resume} element={<Resume />} />
+            <Route path={ROUTES.map} element={<Map />} />
+            <Route path={ROUTES.timer} element={<Timer />} />
+            <Route path={ROUTES.all} element={<Navigate to={ROUTES.index} replace />} />
+            <Route index element={<Resume />} />
+          </Route>
+        </Routes>
+      </Router>
+    </TimerContext.Provider>
   );
 };
 
--- src/pages/Timer/Timer.tsx
+++ src/pages/Timer/Timer.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import BasicBlock from '@components/BasicBlock';
+import React, { useContext, useEffect, useState } from 'react';
+import getTime from '@utils/getTime';
+import { TimerContext } from '@src/App';
 
 const Timer: React.FC = () => {
-  return <section>Timer</section>;
+  const startTime = useContext(TimerContext);
+  const currentTime = Date.now();
+  const diffTime = (currentTime - startTime) / 1000;
+
+  const [seconds, setSeconds] = useState(diffTime);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <BasicBlock title="Timer">
+      <span>{getTime(seconds)}</span>
+    </BasicBlock>
+  );
 };
 
-export default Timer;
+export default React.memo(Timer);
